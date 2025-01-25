f2416b02b7b01a7e6283591f5a3fcb6afd958662
Lodney Santos <a21505293@alunos.ulht.pt>
2025-01-25 18:43:19 +0000

    Add global CSS styles and favicon; remove old manifest and robots.txt files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import './assets/styles/global.css'
 
 // Renderizar os componentes na DOM do navegador
 // Render the components in the browser's DOM
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1> Bem-vindo à Home</h1>;
+  return <h1> Bem-vindo à home</h1>;
 };
 
 export default Home;
