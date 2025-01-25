eac82e5925fcca3cf4ba9503cba7d9f97961481c
Lodney Santos <a21505293@alunos.ulht.pt>
2025-01-25 17:23:04 +0000

    Add react-router-dom for routing; update imports in App component and create About page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 const App = () => {
   return (
