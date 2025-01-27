7ce10346cb1aef1b80178fc6fa449644df1f13ee
Lodney Santos <a21505293@alunos.ulht.pt>
2025-01-27 23:41:58 +0000

    Add About section styling and content: implement layout with overlay text and adjust padding

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -4,7 +4,12 @@ import "./About.css";
 const About = () => {
   return (
     <div className="about-section">
-      
+      <div className="wrapper">
+        <span className="big-title">ABOUT</span>
+        <span className="overlay-vibra-word">VIBRA</span>
+        <span className="overlay-liste-word">LISTE</span>
+        <span className="overlay-bold-n">N</span>
+      </div>
     </div>
   );
 };
