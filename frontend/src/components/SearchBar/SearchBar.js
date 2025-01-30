ada83240db5e48ce99f0ffcdef165a68a985edd6
Lodney Santos <a21505293@alunos.ulht.pt>
2025-01-30 17:53:45 +0000

    Add FAQ section with search functionality and styling

---
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./SearchBar.css"
+
+function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Carregar valor guardado ao iniciar a página
+  // Load saved value when starting the page
+  useEffect(() => {
+    const savedSearch = sessionStorage.getItem("searchTerm");
+    if (savedSearch) {
+      setSearchTerm(savedSearch);
+    }
+  }, []);
+
+  // Atualizar o sessionStorage sempre que o user escrever algo
+  // Update the sessionStorage whenever the user writes something
+  const handleSearchChange = (e) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    sessionStorage.setItem("searchTerm", value);
+  };
+
+  return (
+    <form>
+      <label htmlFor="search">Search</label>
+      <input 
+      id="search"
+      type="search"
+      pattern=".*\S.*"
+      required
+      value={searchTerm}
+      onChange={handleSearchChange}
+      autoComplete="off"
+      />
+      <span className="caret"></span>
+    </form>
+  );
+}
+
+export default SearchBar;
--- frontend/src/pages/FAQs/FAQs.js
+++ frontend/src/pages/FAQs/FAQs.js
@@ -1,9 +1,15 @@
 import React from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
 import "./FAQs.css";
 
 const FAQs = () => {
   return (
     <section className="faqs-section">
+       
+      <header className="faqs-header">
+        <h1 className="faqs-title">FAQs</h1>
+        <SearchBar />
+      </header>
        
     </section>
   );
