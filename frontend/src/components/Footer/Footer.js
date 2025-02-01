d1e2f77e5855294d95408655c2ca0e23e0771f8f
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-01 04:23:35 +0000

    Refactor Footer component: update class names for better clarity and consistency in styling

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -8,19 +8,19 @@ const Footer = () => {
   return (
     <footer>
       {/* Logo */}
-      <div className="logo-container">
+      <div className="footer-logo-container">
           <Link to="/home">
             <img
               src="Logo/music_symbol.svg"
               alt="Music Symbol"
-              className="logo-image"
+              className="footer-logo-image"
             />
-            <div className="logo-text">
-              <p className="slogan ">Listen. Feel. Download.</p>
-              <h1 className="logo">
-                <span className="vibra">VIBRA</span>
-                <span className="liste">LISTE</span>
-                <span className="vibra">N</span>
+            <div className="footer-logo-text">
+              <p className="slogan-footer">Listen. Feel. Download.</p>
+              <h1 className="footer-logo">
+                <span className="footer-vibra">VIBRA</span>
+                <span className="footer-liste">LISTE</span>
+                <span className="footer-vibra">N</span>
               </h1>
             </div>
           </Link>
@@ -28,22 +28,22 @@ const Footer = () => {
 
         {/* Navigation */}
         <nav>
-          <ul className="nav-list">
+          <ul className="nav-list-footer">
           <li>
-              <Link to="/home" className={activePage === "home" ? "nav-item active" : "nav-item"}>Home</Link>
+              <Link to="/home" className={activePage === "home" ? "nav-item-footer active" : "nav-item-footer"}>Home</Link>
             </li>
             <li>
-              <Link to="/about" className={activePage === "about" ? "nav-item active" : "nav-item"}>About</Link>
+              <Link to="/about" className={activePage === "about" ? "nav-item-footer active" : "nav-item-footer"}>About</Link>
             </li>
             <li>
-              <Link to="/faqs" className={activePage === "faqs" ? "nav-item active" : "nav-item"}>FAQs</Link>
+              <Link to="/faqs" className={activePage === "faqs" ? "nav-item-footer active" : "nav-item-footer"}>FAQs</Link>
             </li>
             <li>
-              <Link to="/contact" className={activePage === "contact" ? "nav-item active" : "nav-item"}>Contact</Link>
+              <Link to="/contact" className={activePage === "contact" ? "nav-item-footer active" : "nav-item-footer"}>Contact</Link>
             </li>
           </ul>
         </nav>
-        <div>© 2025 VIBRALISTEN. All rights reserved.</div>
+        <div className="footer-all-rights">© 2025 VIBRALISTEN. All rights reserved.</div>
     </footer>
   );
 }
