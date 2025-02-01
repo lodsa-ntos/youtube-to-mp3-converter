17db29f7b12ce64681a3c4f851fe0c9caa5827c8
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-01 04:35:08 +0000

    Enhance Footer component: add GitHub contribution link, improve navigation structure, and adjust footer height in CSS

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import getActivePage from "../Header/ActiveEffect";
+import { BsGithub } from "react-icons/bs";
 import "../Header/ActiveEffect";
+import "./Footer.css";
 
 const Footer = () => {
   const activePage = getActivePage();
@@ -9,41 +11,57 @@ const Footer = () => {
     <footer>
       {/* Logo */}
       <div className="footer-logo-container">
-          <Link to="/home">
-            <img
-              src="Logo/music_symbol.svg"
-              alt="Music Symbol"
-              className="footer-logo-image"
-            />
-            <div className="footer-logo-text">
-              <p className="slogan-footer">Listen. Feel. Download.</p>
-              <h1 className="footer-logo">
-                <span className="footer-vibra">VIBRA</span>
-                <span className="footer-liste">LISTE</span>
-                <span className="footer-vibra">N</span>
-              </h1>
-            </div>
-          </Link>
-        </div>
+        <Link to="/home">
+          <img
+            src="Logo/music_symbol.svg"
+            alt="Music Symbol"
+            className="footer-logo-image"
+          />
+          <div className="footer-logo-text">
+            <p className="slogan-footer">Listen. Feel. Download.</p>
+            <h1 className="footer-logo">
+              <span className="footer-vibra">VIBRA</span>
+              <span className="footer-liste">LISTE</span>
+              <span className="footer-vibra">N</span>
+            </h1>
+          </div>
+        </Link>
+      </div>
 
-        {/* Navigation */}
-        <nav>
-          <ul className="nav-list-footer">
+      {/* Navigation */}
+      <nav>
+        <ul className="nav-list-footer">
           <li>
-              <Link to="/home" className={activePage === "home" ? "nav-item-footer active" : "nav-item-footer"}>Home</Link>
-            </li>
-            <li>
-              <Link to="/about" className={activePage === "about" ? "nav-item-footer active" : "nav-item-footer"}>About</Link>
-            </li>
-            <li>
-              <Link to="/faqs" className={activePage === "faqs" ? "nav-item-footer active" : "nav-item-footer"}>FAQs</Link>
-            </li>
-            <li>
-              <Link to="/contact" className={activePage === "contact" ? "nav-item-footer active" : "nav-item-footer"}>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="footer-all-rights">© 2025 VIBRALISTEN. All rights reserved.</div>
+            <Link to="/home" className={activePage === "home" ? "nav-item-footer active" : "nav-item-footer"}>Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className={activePage === "about" ? "nav-item-footer active" : "nav-item-footer"}>About</Link>
+          </li>
+          <li>
+            <Link to="/faqs" className={activePage === "faqs" ? "nav-item-footer active" : "nav-item-footer"}>FAQs</Link>
+          </li>
+          <li>
+            <Link to="/contact" className={activePage === "contact" ? "nav-item-footer active" : "nav-item-footer"}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Contribute on GitHub */}
+      <a
+        className="github-link"
+        href="https://github.com/lodsa-ntos/youtube-to-mp3-converter"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="github-space">
+          <div className="github-icon-container">
+            <BsGithub className="github-icon"/> 
+          </div>
+          <p className="contribute-subtitle">Contribute on GitHub</p>
+        </div>
+      </a>
+
+    <div className="footer-all-rights">© 2025 VIBRALISTEN. All rights reserved.</div>
     </footer>
   );
 }
