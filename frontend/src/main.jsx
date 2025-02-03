7b4e945b15a2c210b22988494b5fbe23a0a21cc0
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-03 02:14:27 +0000

    Update index.html and main.jsx to set up root element for React rendering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,8 @@ import './assets/styles/global.css'
 
 // Renderizar os componentes na DOM do navegador
 // Render the components in the browser's DOM
-ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-  <App/>
-</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
