a0399cd1daf4a4f26b0968efdc799e3d57061f88
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-06 21:44:15 +0000

    Update Terms of Use; enhance legal clarity, add sections on intellectual property, liability limitations, and modifications to terms

--- frontend/src/pages/Terms/Terms.jsx
+++ frontend/src/pages/Terms/Terms.jsx
@@ -39,17 +39,51 @@ const Terms = () => {
             <li>User Responsibility</li>
 
             <div>
-              VIBRALISTEN lets you convert public YouTube videos to MP3 quickly
-              and easily.
+              You are solely responsible for your use of the service. Make sure you have the necessary rights or permissions to convert and download YouTube content.
             </div>
 
-            <li>Functionality</li>
+            <li>Legal Use</li>
 
             <div>
-              VIBRALISTEN lets you convert public YouTube videos to MP3 quickly
-              and easily.
+              The service should only be used for legal purposes. Any use that violates copyright, intellectual property laws or other regulations will be the sole responsibility of the user.
             </div>
           </ul>
+
+          <h4>3. Intellectual property</h4>
+          <div>
+          All content, design, graphics, logos and other elements on the website are the property of VIBRALISTEN or its licensors. Unauthorised use of these materials may infringe copyright laws and other proprietary rights.
+          </div>
+
+          <h4>4. Limitation of Liability</h4>
+          <ul className="service-topcs">
+            <li>Conversion and Quality</li>
+
+            <div>
+            VIBRALISTEN endeavours to provide high quality conversions, but does not guarantee that the process will be free of errors or interruptions.
+            </div>
+
+            <li>Exclusion of Liability</li>
+
+            <div>
+            In no event shall VIBRALISTEN be liable for direct, indirect, incidental or consequential damages arising out of the use of the service or the inability to use it.
+            </div>
+
+            <li>Changes to the Service</li>
+
+            <div>
+            We reserve the right to modify or discontinue the service at any time without prior notice.
+            </div>
+          </ul>
+
+          <h4>5. Modifications to the Terms</h4>
+          <div>
+          We may update these Terms of Use from time to time. Changes will be posted on the site and, by continuing to use the service, you agree to the revised terms.
+          </div>
+
+          <h4>Contacting Us</h4>
+          <div>
+          If you have any questions or suggestions about these Terms of Use, please contact us via our contact form or send an e-mail to: support@vibralisten.com.
+          </div>
         </div>
       </main>
       {/* Footer Section*/}
