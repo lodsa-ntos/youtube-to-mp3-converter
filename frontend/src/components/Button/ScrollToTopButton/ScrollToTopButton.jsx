a8f4017093f4b50c9bab276d704d5aca3b94ec44
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-06 17:44:58 +0000

    Add ScrollToTopButton component; implement initial structure and styling

---
+++ frontend/src/components/Button/ScrollToTopButton/ScrollToTopButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./ScrollToTopButton.css";
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible ] = useState(false);
+
+  //
+};
+
+export default ScrollToTopButton;
