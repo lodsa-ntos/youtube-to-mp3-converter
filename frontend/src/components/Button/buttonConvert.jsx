8b1e6243a4a099b1f4d5ac1f7ac50a8be17d73a0
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-06 01:05:24 +0000

    Refactor Button component; implement link validation with visual feedback for valid and invalid inputs

--- frontend/src/components/Button/buttonConvert.jsx
+++ frontend/src/components/Button/buttonConvert.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useState } from "react";
 import "./buttonConvert.css";
 
 const ButtonConvert = () => {
+  const [ isValid, setIsValid ] = useState(null);
+
+  const validateLink = (e) => {
+    const link = e.target.value;
+    const youtubeRegex = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/;
+    
+    if (youtubeRegex.test(link)) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
+
   return (
     <React.Fragment>
     <div className="container-box">
       <div className="box">
-        <input className="link-box" type="text" id="input-link" placeholder="Please paste the YouTube video URL here..."></input>
+        <input className={`link-box ${isValid === null ? "" : isValid ? "valid-input" : "invalid-input"}`} type="text" onChange={validateLink} id="input-link" placeholder="Please paste the YouTube video URL here..."></input>
         <button className="bnt">Convert</button>
       </div>
     </div>
